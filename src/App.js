ea0001e6da87ca14bd658fa4df38387c803538b1
Anzhelika Bielykh <anjelika.belikh@gmail.com>
2025-11-24 23:17:35 +0200

    useRef

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
-import { useReducer, useState } from "react";
+import { useRef } from "react";
 import "./App.css";
 
 function App() {
-  const reducer = (state, action) => {
-    if (action.type === "increment") {
-      return { count: state.count + 1 };
-    }
-    if (action.type === "decrement") {
-      return { count: state.count - 1 };
-    }
-    if (action.type === "reset") {
-      return { count: 0 };
-    }
-  };
-  const [value, dispatch] = useReducer(reducer, { count: 0 });
-
-  const handleClick = (type) => {
-    dispatch(type);
-  };
+  const headerRef = useRef();
+  console.log(headerRef);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>{value.count}</p>
-        <button onClick={() => handleClick({ type: "increment" })}>Add</button>
-        <button onClick={() => handleClick({ type: "decrement" })}>Minus</button>
-        <button onClick={() => handleClick({ type: "reset" })}>Reset</button>
+      <header className="App-header" ref={headerRef}>
+        <p>Text</p>
       </header>
     </div>
   );
