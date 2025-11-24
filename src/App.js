1e39f807ed848ce5f623525e90365b7d17c8d69d
Anzhelika Bielykh <anjelika.belikh@gmail.com>
2025-11-24 22:57:44 +0200

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
-import { useEffect, useState } from "react";
-import UnmountComponent from "./UnmountComponent";
+import { useReducer, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [value, setValue] = useState(0);
-  const [isMounted, setIsMounted] = useState(false);
-
-   // useEffect(() => {
-  //   console.log("ComponentDidMount useEffect");
-  // }, []);
-
-  // useEffect(() => {
-  //   console.log("ComponentDidUpdate useEffect");
-  // }, [value]);
-
-  const handleClick = () => {
-    setValue((prevValue) => prevValue + 1);
-    setValue((prevValue) => prevValue + 1);
-    setValue((prevValue) => prevValue + 1);
-    setValue((prevValue) => prevValue + 1);
-    setValue((prevValue) => prevValue + 1);
-    // setIsMounted(!isMounted);
+  const reducer = (state, action) => {
+    if (action.type === "increment") {
+      return { count: state.count + 1 };
+    }
+    if (action.type === "decrement") {
+      return { count: state.count - 1 };
+    }
+    if (action.type === "reset") {
+      return { count: 0 };
+    }
   };
+  const [value, dispatch] = useReducer(reducer, { count: 0 });
+
+  const handleClick = (type) => {
+    dispatch(type);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>{value}</p>
-        {/* {isMounted ? <UnmountComponent /> : <p>Text</p>} */}
-        <button onClick={handleClick}>Click me</button>
+        <p>{value.count}</p>
+        <button onClick={() => handleClick({ type: "increment" })}>Add</button>
+        <button onClick={() => handleClick({ type: "decrement" })}>Minus</button>
+        <button onClick={() => handleClick({ type: "reset" })}>Reset</button>
       </header>
     </div>
   );
