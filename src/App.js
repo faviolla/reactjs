aed5ff9b0998f5952bfa0a742dde5e0315369dc8
Anzhelika Bielykh <anjelika.belikh@gmail.com>
2025-12-10 23:21:02 +0200

    Catch error.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,20 @@
 import { useEffect, useState } from "react";
-// import PersonList from "./components/PersonList/PersonList";
-// import UncontrolledForm from "./components/UncontrolledForms/UncontrolledForm";
-// import ControlledForms from "./components/ControlledForms/ControlledForms";
-import { getContactsList } from "./api/api";
+// import { getContactsList } from "./api/api";
 import "./App.css";
 import Loader from "./components/Loader/Loader";
+import { useFetch } from "./hooks/useFetch";
 
 function App() {
-  // const inputRef = useRef();
+  const { data: contacts, isLoading, error } = useFetch("contacts");
 
-  // const handleFocus = () => {
-  //   inputRef.current.focus();
-  //   console.log(inputRef.current.value);
-  // };
-
-  // }
-  const [contacts, setContacts] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const fetchData = async () => {
-    setIsLoading(true);
-    const data = await getContactsList();
-    setIsLoading(false);
-    setContacts(data);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  console.log(contacts);
+  if (error) {
+    return <div>something went wrong {error}</div>;
+  }
 
   return (
     <div className="App">
+      <h1>Contacts</h1>
       <header className="App-header">
-        <h1>Contacts</h1>
         {/* <input type="text" ref={inputRef} />
         <p>Text</p>
         <button onClick={handleFocus}>Focus me</button> */}
